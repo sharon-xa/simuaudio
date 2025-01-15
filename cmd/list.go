f17c0e0a7e5e5a07c7700f1d18c1807e637ba1be
sharon <ali93456@gmail.com>
2025-01-15 03:59:06 +0300

    fix the list command not showing built-in speakers

--- cmd/list.go
+++ cmd/list.go
@@ -9,13 +9,49 @@ import (
 )
 
 func ListAudioDevices() {
+	outputStr, err := getDevices()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	lines := strings.Split(outputStr, "\n")
+	devices := make(map[string]string)
+
+	i := 0
+	for i < len(lines) {
+		line := strings.TrimSpace(lines[i])
+		nextLine := ""
+		if i != len(lines)-1 {
+			nextLine = strings.TrimSpace(lines[i+1])
+		}
+
+		if strings.HasPrefix(line, "node.name") && strings.HasPrefix(nextLine, "media.name") {
+			devices[extractValue(nextLine)] = extractValue(line)
+		} else if strings.HasPrefix(line, "node.name") && (strings.HasPrefix(nextLine, "node.name") || nextLine == "") {
+			devices["Built-In Audio (speakers or audio jack)"] = extractValue(line)
+		}
+		i++
+	}
+
+	if len(devices) <= 0 {
+		fmt.Println("No device found")
+		return
+	}
+
+	arrow := "\033[1;31m->\033[0m" // Red and bold
+	for name, nodeName := range devices {
+		fmt.Printf("%s %s %s\n", name, arrow, nodeName)
+	}
+}
+
+func getDevices() (string, error) {
 	cmd := exec.Command("pactl", "list", "sinks")
 	find := exec.Command("grep", "-E", "node.name|media.name")
 
 	pipeReader, pipeWriter, err := os.Pipe()
 	if err != nil {
-		fmt.Println("PIPE Error:", err)
-		return
+		return "", err
 	}
 
 	// Connect cmd's stdout to the pipe's writer
@@ -32,47 +68,22 @@ func ListAudioDevices() {
 	if err := cmd.Start(); err != nil {
 		panic(err)
 	}
+
 	if err := find.Start(); err != nil {
 		panic(err)
 	}
 
-	// Close the writer when cmd finishes to signal EOF to find
+	// Close the writer when "cmd" finishes to signal EOF to "find"
 	go func() {
 		defer pipeWriter.Close()
 		cmd.Wait()
 	}()
 
-	// Wait for cmd to finish
+	// Wait for "cmd" to finish
 	find.Wait()
 
 	outputStr := strings.TrimSpace(output.String())
-	lines := strings.Split(outputStr, "\n")
-
-	devices := make(map[string]string)
-	var currentNode string
-
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		if strings.HasPrefix(line, "node.name") {
-			currentNode = extractValue(line)
-		} else if strings.HasPrefix(line, "media.name") {
-			mediaName := extractValue(line)
-			if currentNode != "" {
-				devices[mediaName] = currentNode
-				currentNode = "" // reset
-			}
-		}
-	}
-
-	if len(devices) <= 0 {
-		fmt.Println("No device found")
-		return
-	}
-
-	arrow := "\033[1;31m->\033[0m" // Red and bold
-	for name, nodeName := range devices {
-		fmt.Printf("%s %s %s\n", name, arrow, nodeName)
-	}
+	return outputStr, nil
 }
 
 // Helper function to extract value from a line like `key = "value"`
